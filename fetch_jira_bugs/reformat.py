f6124d46da1ca42aff42b6566eafb1eb6ca4a540
ShoOgino <loginologist@gmail.com>
2021-06-01 23:35:09 +0900

    - enable to merge small annotation files(by historage) - enable to reformat correctly

--- fetch_jira_bugs/reformat.py
+++ fetch_jira_bugs/reformat.py
@@ -2,21 +2,33 @@ import json
 import argparse
 import os
 
-def reformat(pathInput, pathOutput):
-    print(pathInput)
+def reformat(pathAnnotation, pathBugFixes, pathOutput):
+    print(pathAnnotation)
+    print(pathBugFixes)
     print(pathOutput)
     historyBugFile={}
-    with open(pathInput) as f:
+    idBugReport2idFix={}
+    with open(pathBugFixes) as f:
+        idBugReport2idFix = json.load(f)
+
+    with open(pathAnnotation) as f:
         df = json.load(f)
         for commitFix in df:
             for item in df[commitFix]:
                 if(1<len(item["revisions"])):
                     if(not item["filePath"] in historyBugFile):
                         historyBugFile[item["filePath"]]={}
-                    if(not commitFix in historyBugFile[item["filePath"]]):
-                        historyBugFile[item["filePath"]][commitFix]=[]
+                    #ここでバグレポートidを入れる
+                    idBugReport=""
+                    for key in idBugReport2idFix:
+                        if(idBugReport2idFix[key]["hash"]==commitFix):
+                            idBugReport=key.split('_')[0]
+                    if(not idBugReport in historyBugFile[item["filePath"]]):
+                        historyBugFile[item["filePath"]][idBugReport]={}
+                    if(not commitFix in historyBugFile[item["filePath"]][idBugReport]):
+                        historyBugFile[item["filePath"]][idBugReport][commitFix]=[]
                     revisions=[i for i in item["revisions"] if i!=commitFix]
-                    historyBugFile[item["filePath"]][commitFix].extend(revisions)
+                    historyBugFile[item["filePath"]][idBugReport][commitFix].extend(revisions)
                     #if(commitFix != item["revisions"][0]):
                     #    print(item["filePath"])
                     #    print(commitFix)
@@ -43,11 +55,13 @@ def getPathOutput(path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="""Convert a git log output to json.""")
-    parser.add_argument('--pathInput', type=str, default="")
+    parser.add_argument('--pathAnnotation', type=str, default="")
+    parser.add_argument('--pathBugFixes', type=str, default="")
     parser.add_argument('--pathOutput', type=str, default="", help="dest")
 
     args = parser.parse_args()
-    pathInput = getPathInput(args.pathInput)
+    pathAnnotation = getPathInput(args.pathAnnotation)
+    pathBugFixes = getPathInput(args.pathBugFixes)
     pathOutput = getPathOutput(args.pathOutput)
 
-    reformat(pathInput, pathOutput)
+    reformat(pathAnnotation, pathBugFixes, pathOutput)
